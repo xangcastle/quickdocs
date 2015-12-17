d2e257061a48e93f97d0ea5c63913ca5f654e38d
xangmuve <cesarabel@johnmay.com.ni>
2015-12-17 09:17:31 -0600

    cambio de entorno grafico de interface de usuario

--- quickdocs/settings.py
+++ quickdocs/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'adminactions',
     'import_export',
+    'grappelli_dynamic_navbar',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
