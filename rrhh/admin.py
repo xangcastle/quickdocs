ed414933cffe81584ae10b5eaca4ef04996c4f49
xangmuve <cesarabel@johnmay.com.ni>
2016-08-30 10:52:31 -0600

    new changes

--- rrhh/admin.py
+++ rrhh/admin.py
@@ -3,6 +3,9 @@ from .models import *
 
 
 class person_admin(admin.ModelAdmin):
-    list_display = ('cedula', 'nombre', 'hora')
+    list_display = ('cedula', 'nombre', 'salario', 'fecha_ingreso')
+    search_fields = ('cedula', 'nombre')
+    list_filter = ('salario',)
+    fields = ('nombre', ('cedula', 'salario'), 'fecha_ingreso')
 
 admin.site.register(Person, person_admin)
