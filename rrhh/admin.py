0f3a74d2448fb55c1f417789257292f19a5bfa0c
xangmuve <cesarabel@johnmay.com.ni>
2016-09-07 13:36:09 -0600

    planilla mes

--- rrhh/admin.py
+++ rrhh/admin.py
@@ -13,40 +13,56 @@ class person_admin(admin.ModelAdmin):
     search_fields = ('cedula', 'nombre')
     list_filter = ('salario',)
     fields = ('nombre', ('cedula', 'salario'), 'fecha_ingreso',
-        ('moneda', 'tc'), 'cuenta', ('deducciones', ))
-    actions = ['generar_planilla',]
+        ('moneda', 'tc'), 'cuenta')
+    actions = ['planilla_dia', 'planilla_mes']
 
     class rango_fecha(forms.Form):
         _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
         fecha_inicial = forms.DateField(widget=widgets.AdminDateWidget())
-        fecha_final = forms.DateField(
-            widget=widgets.AdminDateWidget())
+        fecha_final = forms.DateField(widget=widgets.AdminDateWidget())
 
-    def generar_planilla(self, request, queryset):
+    class config_mes(forms.Form):
+        MESES = (
+        ("ENERO", "ENERO"),
+        ("FEBRERO", "FEBRERO"),
+        ("MARZO", "MARZO"),
+        ("ABRIL", "ABRIL"),
+        ("MAYO", "MAYO"),
+        ("JUNIO", "JUNIO"),
+        ("JULIO", "JULIO"),
+        ("AGOSTO", "AGOSTO"),
+        ("SEPTIEMBRE", "SEPTIEMBRE"),
+        ("OCTUBRE", "OCTUBRE"),
+        ("NOVIEMBRE", "NOVIEMBRE"),
+        ("DICIEMBRE", "DICIEMBRE"),
+        )
+        QUINSENAS = (
+        ("PRIMERA", "PRIMERA"),
+        ("SEGUNDA", "SEGUNDA"),
+        ("AMBAS", "AMBAS"),
+        )
+        _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+        mes = forms.ChoiceField(choices=MESES)
+        quinsena = forms.ChoiceField(choices=QUINSENAS)
+
+    def planilla_dia(self, request, queryset):
         message = ""
         form = None
         data = {}
         data['header_tittle'] = 'Por Favor complete todos los campos'
         data['explanation'] = 'Las Siguientes personas estan incluidas en esta planilla:'
-        data['action'] = 'generar_planilla'
+        data['action'] = 'planilla_dia'
         if 'calcular' in request.POST:
             form = self.rango_fecha(request.POST)
             if form.is_valid():
                 fecha_inicial = datetime.strptime(request.POST.get('fecha_inicial', ''), "%d/%m/%Y")
                 fecha_final = datetime.strptime(request.POST.get('fecha_final', ''), "%d/%m/%Y")
-                data['dias'] = (fecha_final - fecha_inicial).days
-                print data['dias']
-                data['queryset'] = [x.pago(data['dias']) for x in queryset]
+                data['dias'] = (fecha_final - fecha_inicial).days + 1
+                data['queryset'] = [x.pago_dias(data['dias']) for x in queryset]
                 data['header_tittle'] = 'Detalle de Pago de Planilla del periodo entre %s y %s' % (str(fecha_inicial), str(fecha_final))
                 data['explanation'] = 'Se requerira Autorizacion de Gerencia para aplicar las Notas de Credito.'
                 data.update(csrf(request))
-                return render_to_response('rrhh/planilla_settings.html', data, RequestContext(request))
-
-        elif 'aplicar' in request.POST:
-            print "prueba 1"
-            print request.POST.getlist('importe', '')
-            data.update(csrf(request))
-            return render_to_response('rrhh/planilla_settings.html', data, RequestContext(request))
+                return render_to_response('rrhh/pago_dias.html', data, RequestContext(request))
 
         if not form:
             form = self.rango_fecha(
@@ -57,8 +73,41 @@ class person_admin(admin.ModelAdmin):
         data['form'] = form
         data.update(csrf(request))
         self.message_user(request, message)
-        return render_to_response('rrhh/planilla_settings.html', data, RequestContext(request))
+        return render_to_response('rrhh/pago_dias.html', data, RequestContext(request))
+    planilla_dia.short_description = "Generar planilla de corte por Dia"
 
+    def planilla_mes(self, request, queryset):
+        message = ""
+        form = None
+        data = {}
+        data['header_tittle'] = 'Por Favor complete todos los campos'
+        data['explanation'] = 'Las Siguientes personas estan incluidas en esta planilla:'
+        data['action'] = 'planilla_mes'
+        if 'calcular' in request.POST:
+            form = self.config_mes(request.POST)
+            if form.is_valid():
+                mes = request.POST.get('mes', '')
+                quinsena = request.POST.get('quinsena', '')
+                if quinsena == "AMBAS":
+                    data['header_tittle'] = 'Detalle de Pago de Planilla del mes de %s' % (mes)
+                else:
+                    data['header_tittle'] = 'Detalle de Pago de Planilla del mes de %s %s quinsena' % (mes, quinsena)
+                data['mes'] = mes
+                data['queryset'] = [x.pago_mes(quinsena, mes) for x in queryset]
+                data['explanation'] = 'Se requerira Autorizacion de Gerencia para aplicar las Notas de Credito.'
+                data.update(csrf(request))
+                return render_to_response('rrhh/pago_mes.html', data, RequestContext(request))
+
+        if not form:
+            form = self.config_mes(
+                initial={
+                    '_selected_action': request.POST.getlist(
+                        admin.ACTION_CHECKBOX_NAME)})
+        data['queryset'] = queryset
+        data['form'] = form
+        data.update(csrf(request))
+        self.message_user(request, message)
+        return render_to_response('rrhh/pago_mes.html', data, RequestContext(request))
+    planilla_dia.short_description = "Generar planilla de corte por Dia"
 
 admin.site.register(Person, person_admin)
-admin.site.register(Deduccion)
--- rrhh/models.py
+++ rrhh/models.py
@@ -12,49 +12,49 @@ class Person(models.Model):
     salario = models.FloatField(null=True)
     tc = models.FloatField(null=True, default=1.0, verbose_name="tasa de cambio")
     cuenta = models.CharField(max_length=25, null=True, verbose_name="cuenta a aplicar",
-    help_text="cuenta banpro cordobas")
+    help_text="cuenta banpro")
     fecha_ingreso = models.DateField(null=True, blank=True)
-    deducciones = models.ManyToManyField('Deduccion', null=True, blank=True)
 
     def __unicode__(self):
         return self.nombre
 
-    def pago(self, dias):
+    def ir(self, monto):
+        return round(monto * 0.1, 2)
+
+    def inss(self, monto):
+        return round(monto * 0.0625, 2)
+
+    def patronal(self, monto):
+        return round(monto * 0.185, 2)
+
+    def inatec(self, monto):
+        return round(monto * 0.02, 2)
+
+    def pago_dias(self, dias):
         tarifa = round(self.salario / 30, 2)
         importe = round(tarifa * dias, 2)
-        retension = round(importe * 0.1)
-        return {
-        'id': self.id,
-        'cedula': self.cedula,
-        'nombre': self.nombre,
-        'salario': self.salario,
-        'tarifa': tarifa,
-        'importe': importe,
-        'retension': retension,
-        'deducciones': 0.0,
-        'monto': round(importe - retension, 2)
-        }
+        inss = self.inss(importe)
+        ir = self.ir(importe - inss)
+        inatec = self.inatec(importe)
+        return {'id': self.id, 'cedula': self.cedula, 'nombre': self.nombre,
+        'salario': self.salario, 'tarifa': tarifa, 'importe': importe,
+        'inss': inss, 'patronal': self.patronal(importe), 'inatec': inatec,
+        'ir': ir, 'monto': round(importe - (inss + ir + inatec), 2)}
+
+    def pago_mes(self, quinsena, mes):
+        if quinsena == "AMBAS":
+            importe = self.salario
+        else:
+            importe = round(self.salario / 2, 2)
+        inss = self.inss(importe)
+        ir = self.ir(importe - inss)
+        inatec = self.inatec(importe)
+        return {'id': self.id, 'cedula': self.cedula, 'nombre': self.nombre,
+        'salario': self.salario, 'importe': importe,
+        'inss': inss, 'patronal': self.patronal(importe), 'inatec': inatec,
+        'ir': ir, 'monto': round(importe - (inss + ir + inatec), 2)}
+
 
     class Meta:
         verbose_name = "Persona"
         verbose_name_plural = "Servicios Profesionales"
-
-
-class Deduccion(models.Model):
-    APLICABLES = (
-    ("EMPLEADO", "EMPLEADO"),
-    ("EMPLEADOR", "EMPLEADOR"),
-    )
-    nombre = models.CharField(max_length=65)
-    porcent = models.FloatField(verbose_name="porcentaje a aplicar %")
-    auxiliar = models.CharField(max_length=4, null=True)
-    cuenta = models.CharField(max_length=25, null=True,
-    verbose_name="cuenta contable")
-    aplica = models.CharField(max_length=15, choices=APLICABLES,
-    default="EMPLEADO")
-
-    class Meta:
-        verbose_name_plural = "deducciones"
-
-    def __unicode__(self):
-        return ("%s %s" % (self.nombre, str(round(self.porcent, 2)))) + "%"
