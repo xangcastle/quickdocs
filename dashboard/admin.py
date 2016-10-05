a5a322e88beef8dfe0d8aa3ce53d2699e91f9040
xangmuve <cesarabel@johnmay.com.ni>
2016-10-05 10:48:59 -0600

    modelo para graficos del dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+admin.site.register(Emboso, ImportExportModelAdmin)
