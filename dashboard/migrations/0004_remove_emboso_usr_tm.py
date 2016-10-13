8ebc6a3c7818c8fdd7cba92b17cf3cd7784c42fd
xangmuve <cesarabel@johnmay.com.ni>
2016-10-13 11:10:22 -0600

    correcion del campo

---
+++ dashboard/migrations/0004_remove_emboso_usr_tm.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dashboard', '0003_emboso_usr_tm'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='emboso',
+            name='USR_TM',
+        ),
+    ]
